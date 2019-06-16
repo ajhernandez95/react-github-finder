9a2143fc4695789fce742b7cd68f8fcfd50dbac2
ajhernandez95 <31139761+ajhernandez95@users.noreply.github.com>
2019-06-15 23:34:43 -0500

    Build

---
+++ build/precache-manifest.1783dd284762db5d0b617d32dd4b2195.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff26a6225d87bbf4402000590e852294",
+    "url": "/index.html"
+  },
+  {
+    "revision": "daa13a18d0667aa56d85",
+    "url": "/static/css/main.68a2647f.chunk.css"
+  },
+  {
+    "revision": "2b8cee35225a25ba4c50",
+    "url": "/static/js/2.f22b0e30.chunk.js"
+  },
+  {
+    "revision": "daa13a18d0667aa56d85",
+    "url": "/static/js/main.6e41939c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
